497f5914700c56e249d793ca67ac3d316adc872d
Ace <kronicrespawn@gmail.com>
2022-09-14 21:51:47 -0500

    drop down function added

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
 // create drop down menu
-const menuItem = document.querySelector('li')
+const menuItem = document.querySelector('.about')
+const dropDown = document.querySelector('#dropdown')
+// dropDown.style.display = 'none'
 
 menuItem.addEventListener('click', e => {
-    const pick = e.target
-    const elementD = document.createElement('div')
-    const elementDrop = document.createElement('div')
-    console.log(pick)
-    elementD.innerHTML = ''
-    pick.appendChild(elementD)
-    elementD.appendChild(elementDrop)
-    elementDrop.innerHTML = dropTemplate
-    
+        dropDown.style.display = ''; 
+        console.log('click')
 })
-
-const dropTemplate = `<div class="dropdown">
-    <li> A </li>
-    <li> B </li>
-    <li> C </li>
-</div>
-`
-
-
-// listsContainer.addEventListener('click', e => {
-//     if (e.target.tagName.toLowerCase() === 'li') {
-//         selectedListId = e.target.dataset.listId
-//         saveAndRender()
-//         console.log(selectedListId)
-//         listDisplayContainer.style.display = ''
-//     }
-// })
-
-
-function clearElement(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
-}
