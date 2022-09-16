b01823fc03c362874343841150382f7883fc16cc
Ace <kronicrespawn@gmail.com>
2022-09-15 21:03:13 -0500

    drop down fixed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // create drop down menu
-const menuItem = document.querySelector('.about')
+const menuItem = document.querySelector('.project')
 const dropDown = document.querySelector('#dropdown')
-// dropDown.style.display = 'none'
 
-menuItem.addEventListener('click', e => {
-        dropDown.style.display = ''; 
+menuItem.addEventListener('mouseover', e => {
+        dropDown.style.display = 'block'; 
         console.log('click')
+})
+
+menuItem.addEventListener('mouseout', e => {
+        dropDown.style.display = 'none';
 })
